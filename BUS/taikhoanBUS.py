6d2adafd1301d19f17ce89ed99f3f44a97a5cf13
ImJustV <trandaithang321@gmail.com>
2025-04-18 08:40:11 +0700

    modified:   main.py

--- BUS/taikhoanBUS.py
+++ BUS/taikhoanBUS.py
@@ -6,7 +6,7 @@ class taikhoanBUS:
         self.userDao = nguoidungDAO()
         self.accDao = taikhoanDAO()
         
-    def taoTaiKhoan(self,signUpData:Dict) -> Dict:
+    def taoTaiKhoanND(self,signUpData:Dict) -> Dict:
         # dictionary sẽ có 4 dữ liệu đầu là data tài khoản các dữ liệu sau nó là thông tin người dùng
         for x in signUpData.values():
             if not x:
@@ -20,12 +20,29 @@ class taikhoanBUS:
             result.append(self.accDao.xoa_TaiKhoan(result.get("idTaiKhoan")))
         return result[len(result)-1]
     
+    def taoTaiKhoanNV(self,signUpData:Dict) -> Dict:
+        # dictionary sẽ có 4 dữ liệu đầu là data tài khoản các dữ liệu sau nó là thông tin người dùng
+        for x in signUpData.values():
+            if not x:
+                return {"success": False, "error": "Thiếu thông tin bắt buộc"}
+        result = [] 
+        result.append(self.accDao.timTaiKhoan(signUpData.values()[0]))
+        if not result[0].get("success"):
+            return result
+        result.append(self.accDao.them_TaiKhoan(dict(list(signUpData.items())[:4])))
+        if not result[0].get("success"):
+            return result
+        result.append(self.userDao.them_NguoiDung(dict(list(signUpData.items())[4:])))
+        if not result[1].get("success"):
+            result.append(self.accDao.xoa_TaiKhoan(result.get("idTaiKhoan")))
+        return result[len(result)-1]
+    
     def danhSachTaiKhoan(self):
         return self.accDao.getListTaiKhoan()
     
     def dangNhapTaiKhoan(self,signInData:Dict)->Dict:
         #dictionary đưa vào chỉ có 2 values là tên và mật khẩu
-        result = self.timTaiKhoan(signInData.values()[0])
+        result = self.accDao.timTaiKhoan(signInData.values()[0])
         if result.get("success"):
             return self.accDao.dangNhapTaiKhoan(signInData)
         return result
--- DAO/db_config.py
+++ DAO/db_config.py
@@ -7,6 +7,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="123456",
         database="footballrenting"
     )
